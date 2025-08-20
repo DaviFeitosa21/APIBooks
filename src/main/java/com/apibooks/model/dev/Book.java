8e8cab3f91991083cb04526af39a3ae02063afad
DaviFeitosa <90164085+DaviFeitosa21@users.noreply.github.com>
2025-08-20 18:26:47 -0300

    construindo classe book

--- src/main/java/com/apibooks/model/dev/Book.java
+++ src/main/java/com/apibooks/model/dev/Book.java
@@ -1,5 +1,85 @@
 package com.apibooks.model.dev;
 
-public class Book {
+import java.util.Objects;
 
+public class Book {
+	
+	private Long id_livro;
+	
+	private String titulo;
+	
+	private String autor;
+	
+	private String genero;
+	
+	private String status;
+
+	public Book() {}
+
+	public Book(Long id_livro, String titulo, String autor, String genero, String status) {
+		this.id_livro = id_livro;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.genero = genero;
+		this.status = status;
+	}
+
+	public Long getId_livro() {
+		return id_livro;
+	}
+
+	public void setId_livro(Long id_livro) {
+		this.id_livro = id_livro;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(autor, genero, id_livro, status, titulo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(autor, other.autor) && Objects.equals(genero, other.genero)
+				&& Objects.equals(id_livro, other.id_livro) && Objects.equals(status, other.status)
+				&& Objects.equals(titulo, other.titulo);
+	}
 }
